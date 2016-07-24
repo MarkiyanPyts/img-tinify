09fdc9676e9d56c322f5dfffc2c18a22675b94dd
Markiyan Pyts <markiyan.pyts@osf-global.com>
2016-07-24 20:17:30 +0300

    worked on new API

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    "outputPath": "false",
+    "apiKey": "8lyxmoBum9bHoJwa8dMQtwbTZvLS6mRX"
+}
--- index.js
+++ index.js
@@ -9,28 +9,155 @@ let recursive = require('recursive-readdir');
 process.env.INIT_CWD = process.cwd();
 
 class ImageCompressor {
-    constructor(apiKey, whatIsCount) {
-        if(apiKey) {
-            tinify.key = apiKey;
-            if(whatIsCount == "count") {
-                this.howMuchDidICompress();
-                return false;
+    constructor(userArgs) {
+        this.configName= path.resolve(__dirname + "/config.js");
+        try {
+            this.config = require(this.configName);
+        } catch(err) {
+            this.resetDefaultConfig();
+            try {
+                this.config = require(this.configName);
+            } catch(err) {
+                console.log(err);
             }
-            this.compressImages();
-        }else {
-            console.log("please specify your tinyPng API key");
         }
+
+        this.commands = {
+            "setOutputPath": "setpath",
+            "setApiKey": "setkey",
+            "resetDefaultConfig": "reset",
+            "countOfCompressedImages": "count"
+        }
+        this.configDefaults =  {
+            outputPath: "false",
+            apiKey: ""
+        };
+        if(userArgs.length > 2) {
+            console.log("too many arguments")
+            return false;
+        }
+
+        this.commandName = userArgs[0];
+
+        if(!this.commandName) {
+            tinify.key = this.config.apiKey;
+            this.compressImages();
+        } else {
+            let commandParam;
+            switch (this.commandName) {
+                case this.commands.countOfCompressedImages:
+                    tinify.key = this.config.apiKey;
+                    this.howMuchDidICompress();
+                break;
+                case this.commands.setOutputPath:
+                    commandParam = userArgs[1];
+                    this.setOutputPath(commandParam);
+                break;
+                case this.commands.setApiKey:
+                    commandParam = userArgs[1];
+                    this.setApiKey(commandParam);
+                break;
+                case this.commands.resetDefaultConfig:
+                    this.resetDefaultConfig();
+                break;
+                default:
+                console.log("You misspelled command name");
+                return;
+            }
+        }
+    }
+
+    resetDefaultConfig() {
+        var configDefaults = this.configDefaults;
+        fs.open(this.configName, "w", (err, fd) => {
+            if (err) return console.log(err)
+            fs.writeFile(this.configName, "module.exports = \n"+JSON.stringify(this.configDefaults, null, 4), function (err) {
+                if (err) return console.log(err);
+                console.log("Default configs are set");
+            });
+        });
+    }
+
+    setConfig(name, value) {
+        let newConfig = {};
+        newConfig[name] = value;
+        let mergedConfig = Object.assign({}, this.config, newConfig);
+
+        fs.writeFile(this.configName, "module.exports = \n"+JSON.stringify(mergedConfig, null, 4), (err) => {
+          if (err) return console.log(err)
+          console.log(`${name} is set to ${value}`);
+        });
+    }
+
+    setOutputPath(path) {
+        if(!path) {
+            console.log("Please specify a path");
+            return;
+        }
+        this.setConfig("outputPath", path);
+    }
+
+    setApiKey(key) {
+        if(!key) {
+            console.log("Please specify an API key");
+            return;
+        }
+        this.setConfig("apiKey", key);
     }
 
     howMuchDidICompress() {
         tinify.validate(function(err) {
-          if (!err) {
-              console.log("you optimized " + tinify.compressionCount + " images this month", "max number of free images is 500");
-          }
-        })
+            if (err) {
+              console.log("Your API key is not yet set, or it's invalid");
+              return;
+            } else {
+              console.log(`you optimized ${tinify.compressionCount} images this month, max number of free images is 500`);
+            }
+        });
     }
 
     compressImages() {
+        tinify.validate((err) => {
+            if (err) {
+              console.log(`Your API key is not yet set, or it's invalid, you can set it with "img-tinyfy ${this.commands.setApiKey} YOURKEYHERE" command`);
+              return;
+            } else {
+                if(this.config.outputPath === "false") {
+                    this.compressionStart(false);
+                } else {
+                    try {
+                        let outDirNormalized = path.normalize(this.config.outputPath);
+                        let stats = fs.statSync(outDirNormalized);
+                        if(stats.isDirectory()) {
+                            this.compressionStart(outDirNormalized);
+                        } else {
+                            console.log(`The output path set in config is not a directory, you can use "img-tinyfy ${this.commands.setOutputPath} OUTPUTPATHHERE" to set it properly, if you set path to false images will be compressed in their location`);
+                        }
+                    } catch(err) {
+                        console.log(`The output path set in config is not a directory, you can use "img-tinyfy ${this.commands.setOutputPath} OUTPUTPATHHERE" to set it properly, if you set path to false images will be compressed in their location`)
+                        return;
+                    }
+                }
+            }
+        });
+    }
+
+    createOutputImagesArray(outputPath, inputImages) {
+        if(!outputPath) {
+            return inputImages;
+        }else {
+            let outputImages = [];
+            let outputDirPath;
+            inputImages.forEach((imagePath) => {
+                imagePath = path.normalize(imagePath);
+                outputDirPath = imagePath.replace(path.normalize(process.env.INIT_CWD), outputPath)
+                outputImages.push(outputDirPath);
+            });
+            return outputImages;
+        }
+    }
+
+    compressionStart(outputPath) {
         function ignoreFunc(file, stats) {
             if(path.basename(file).match(/.*\.jpg$/g) || path.basename(file).match(/.*\.png$/g) || stats.isDirectory()) {
                 return false;
@@ -38,9 +165,24 @@ class ImageCompressor {
                 return true;
             }
         }
-        recursive(process.env.INIT_CWD, [ignoreFunc], function (err, files) {
-            files.forEach((name) => {
-                var source = tinify.fromFile(name).toFile(name, function(err) {
+
+        recursive(process.env.INIT_CWD, [ignoreFunc], (err, files) => {
+            if(!files.length) {
+                console.log("There is no .png nor .jpg images in current folder");
+                return;
+            }
+            let OutputImagesArray = this.createOutputImagesArray(outputPath, files);
+
+            files.forEach((name, index) => {
+                if(this.config.outputPath !== "false") {
+                    try {
+                        fs.mkdirSync(path.parse(OutputImagesArray[index]).dir); //create directory if it does not exist
+                    } catch(err) {
+
+                    }
+                }
+
+                var source = tinify.fromFile(name).toFile(OutputImagesArray[index], function(err) {
                     if(err) {
                         if (err instanceof tinify.AccountError) {
                             console.log("Verify your API key and account limit.");
@@ -63,4 +205,5 @@ class ImageCompressor {
 }
 
 let userArgs = process.argv.slice(2);
-let compressor = new ImageCompressor(userArgs[0], userArgs[1]);
+
+let compressor = new ImageCompressor(userArgs);
