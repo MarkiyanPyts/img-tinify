13b8586b7ac8e694db6ceba5f7171c2bdc0c1d0a
Markiyan Pyts <markiyan.pyts@osf-global.com>
2016-10-06 17:39:05 +0300

    Worked on updated functionality

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports =
 {
     "outputPath": "false",
-    "apiKey": ""
+    "apiKey": "8lyxmoBum9bHoJwa8dMQtwbTZvLS6mRX",
+    "iterationNumber": 2,
+    "iterationCheckInterval": 120000,
+    "iterationTimeout": 240000,
+    "errorLogName": "error.log",
+    "notOptimizedImagesLog": "not_optimized_images.log",
+    "notOptimizedImagesFolder": "/notOptimizedImages"
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 "use strict";
 
 let path = require("path");
-let fs = require('fs');
+let fs = require('graceful-fs');
 let tinify = require("tinify");
 let recursive = require('recursive-readdir');
 
@@ -27,11 +27,19 @@ class ImageCompressor {
             "setApiKey": "setkey",
             "resetDefaultConfig": "reset",
             "countOfCompressedImages": "count"
-        }
+        };
+
         this.configDefaults =  {
             outputPath: "false",
-            apiKey: ""
+            apiKey: "",
+            iterationNumber: 2, //number of images to process at once
+            iterationCheckInterval: 120000,// 2 minutes
+            iterationTimeout: 240000, // 4 minutes
+            errorLogName: "error.log",
+            notOptimizedImagesLog: "not_optimized_images.log",
+            notOptimizedImagesFolder: "/notOptimizedImages"
         };
+
         if(userArgs.length > 2) {
             console.log("too many arguments")
             return false;
@@ -67,6 +75,17 @@ class ImageCompressor {
         }
     }
 
+    getFilesizeInBytes(filename) {
+        let stats = fs.statSync(filename);
+        let fileSizeInBytes = stats["size"];
+
+        if(!fileSizeInBytes) {
+            throw "Could not determine file size of " + filename;
+        }
+
+        return fileSizeInBytes;
+    }
+
     resetDefaultConfig() {
         var configDefaults = this.configDefaults;
         fs.open(this.configName, "w", (err, fd) => {
@@ -75,6 +94,24 @@ class ImageCompressor {
                 if (err) return console.log(err);
                 console.log("Default configs are set");
             });
+        });
+    }
+
+    writeLog(logMessage, logType) {
+        let logName;
+        let notOptimizedFolder = this.config.notOptimizedFolder;
+        let currentDate = new Date();
+
+        if(logType === "error") {
+            logName = path.resolve(process.env.INIT_CWD + "/" + this.config.errorLogName);
+        } else if(logType === "not optimized") {
+            logName = path.resolve(process.env.INIT_CWD + "/" + this.config.notOptimizedImagesLog);
+        }
+
+        fs.appendFile(logName, "\n" + currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear() + " " + currentDate.getUTCHours() + ":" +currentDate.getUTCMinutes() + ": " + logMessage, (err) => {
+            if (err) {
+                console.log(err);
+            }
         });
     }
 
@@ -108,8 +145,13 @@ class ImageCompressor {
     howMuchDidICompress() {
         tinify.validate(function(err) {
             if (err) {
-              console.log("Your API key is not yet set, or it's invalid");
-              return;
+                if(err.message.indexOf("Error while connecting") !== -1) {
+                    console.log("You seem to have trouble with network connection");
+                }else {
+                    console.log("Your API key is not yet set, or it's invalid");
+                }
+
+                return;
             } else {
               console.log(`you optimized ${tinify.compressionCount} images this month, max number of free images is 500`);
             }
@@ -119,8 +161,13 @@ class ImageCompressor {
     compressImages() {
         tinify.validate((err) => {
             if (err) {
-              console.log(`Your API key is not yet set, or it's invalid, you can set it with "img-tinyfy ${this.commands.setApiKey} YOURKEYHERE" command`);
-              return;
+                if(err.message.indexOf("Error while connecting") !== -1) {
+                    console.log("You seem to have trouble with network connection");
+                }else {
+                    console.log(`Your API key is not yet set, or it's invalid, you can set it with "img-tinyfy ${this.commands.setApiKey} YOURKEYHERE" command`);
+                }
+
+                return;
             } else {
                 if(this.config.outputPath === "false") {
                     this.compressionStart(false);
@@ -142,19 +189,99 @@ class ImageCompressor {
         });
     }
 
-    createOutputImagesArray(outputPath, inputImages) {
-        if(!outputPath) {
-            return inputImages;
+    createImagesArray(outputPath, inputImages, isOutputArray) {
+        if(!isOutputArray) {
+            isOutputArray = false;
+        }
+
+        let outputImages = [];
+        let outputDirPath;
+        let imageSize;
+
+        if(isOutputArray) {
+            if(!outputPath) {
+                inputImages.forEach((imagePath) => {
+                    imagePath = path.normalize(imagePath);
+
+                    outputImages.push({
+                        path: imagePath
+                    });
+                });
+
+                return outputImages;
+            }else {
+                inputImages.forEach((imagePath) => {
+                    imagePath = path.normalize(imagePath);
+                    outputDirPath = imagePath.replace(path.normalize(process.env.INIT_CWD), outputPath);
+
+                    outputImages.push({
+                        path: outputDirPath
+                    });
+                });
+
+                return outputImages;
+            }
         }else {
-            let outputImages = [];
-            let outputDirPath;
             inputImages.forEach((imagePath) => {
                 imagePath = path.normalize(imagePath);
-                outputDirPath = imagePath.replace(path.normalize(process.env.INIT_CWD), outputPath)
-                outputImages.push(outputDirPath);
+                imageSize = this.getFilesizeInBytes(imagePath);
+
+                outputImages.push({
+                    path: imagePath,
+                    size: imageSize
+                });
             });
+
             return outputImages;
         }
+    }
+
+    optimizeIteration(iterationArray, callback) {
+        if(!this.inputImagesArray.length || !this.outputImagesArray.length) {
+            console.log("Optimization Finished!!!");
+            return false;
+        }
+        console.log("Images optimization is in progress...");
+
+        while(this.iterationInputArray.length !== this.config.iterationNumber || this.inputImagesArray.length === 0) {
+            this.iterationInputArray.push(this.inputImagesArray.shift());
+        }
+
+        while(this.iterationOutputArray.length !== this.config.iterationNumber || this.outputImagesArray.length === 0) {
+            this.iterationOutputArray.push(this.outputImagesArray.shift());
+        }
+
+        this.writeLog("hi", "error");
+        setTimeout(() => {
+            console.log("boom")
+        }, 5000)
+        /*this.iterationInputArray.forEach((currentImage, index) => {
+            if(this.config.outputPath !== "false") {
+                try {
+                    fs.mkdirSync(path.parse(this.iterationOutputArray[index].path).dir); //create directory if it does not exist
+                } catch(err) {
+
+                }
+            }
+
+            var source = tinify.fromFile(currentImage.path).toFile(this.iterationOutputArray[index].path, function(err) {
+                if(err) {
+                    if (err instanceof tinify.AccountError) {
+                        console.log("Verify your API key and account limit.");
+                    } else if (err instanceof tinify.ClientError) {
+                        console.log("Check your source image and request options.");
+                    } else if (err instanceof tinify.ServerError) {
+                        console.log("Temporary issue with the Tinify API.");
+                    } else if (err instanceof tinify.ConnectionError) {
+                        console.log("A network connection error occurred.");
+                    } else {
+                        console.log("The error message is: " + err.message);
+                    }
+                }else {
+                    console.log(currentImage.path + " is optimized");
+                }
+            });
+        });*/
     }
 
     compressionStart(outputPath) {
@@ -171,35 +298,16 @@ class ImageCompressor {
                 console.log("There is no .png nor .jpg images in current folder");
                 return;
             }
-            let OutputImagesArray = this.createOutputImagesArray(outputPath, files);
 
-            files.forEach((name, index) => {
-                if(this.config.outputPath !== "false") {
-                    try {
-                        fs.mkdirSync(path.parse(OutputImagesArray[index]).dir); //create directory if it does not exist
-                    } catch(err) {
+            this.iterationInputArray = [];
+            this.iterationOutputArray = [];
+            this.inputImagesArray = this.createImagesArray(outputPath, files);
+            this.outputImagesArray = this.createImagesArray(outputPath, files, true);
 
-                    }
-                }
+            //console.log("in:", this.inputImagesArray)
+            //console.log("out:", this.outputImagesArray)
 
-                var source = tinify.fromFile(name).toFile(OutputImagesArray[index], function(err) {
-                    if(err) {
-                        if (err instanceof tinify.AccountError) {
-                            console.log("Verify your API key and account limit.");
-                        } else if (err instanceof tinify.ClientError) {
-                            console.log("Check your source image and request options.");
-                        } else if (err instanceof tinify.ServerError) {
-                            console.log("Temporary issue with the Tinify API.");
-                        } else if (err instanceof tinify.ConnectionError) {
-                            console.log("A network connection error occurred.");
-                        } else {
-                            console.log("The error message is: " + err.message);
-                        }
-                    }else {
-                        console.log(name + " is optimized");
-                    }
-                });
-            });
+            this.optimizeIteration();
         });
     }
 }
