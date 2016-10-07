890fa9418c4fbe1597e7f84d352fe9b548b62b4f
Markiyan Pyts <markiyan.pyts@osf-global.com>
2016-10-07 22:51:27 +0300

    removed stuff

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-module.exports =
+module.exports = 
 {
-    "outputPath": "E:\\testGround\\test3Out",
-    "apiKey": "8lyxmoBum9bHoJwa8dMQtwbTZvLS6mRX",
+    "outputPath": "false",
+    "apiKey": "",
     "iterationNumber": 25,
     "iterationCheckInterval": 60000,
     "iterationTimeout": 180000
